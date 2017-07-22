da9d0cc80ecec16e9592b054e2cc5d2ee575ac04
kesavanraja <kesav.kraja@gmail.com>
2017-07-22 17:54:11 +0530

    Update and rename .gitignore to odd or even.c

---
+++ .gitignore/odd or even.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int a;
+printf("Enter an integer: ");
+scanf("%d", &number);
+if(a% 2 == 0)
+{
+printf("%d is even.", number);
+else
+printf("%d is odd.", number);
+}
+return 0;
+}
